f48bf1427f90f9aa1f672fe13c4aa571589387c3
Joao Mago <joao.pedro@natahouse.com>
2020-08-21 18:04:12 -0300

    fix: fixing request for other methods

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ const port = 3000;
 const pointUrl = 'http://localhost:1444';
 
 app.all('*', async (req, res) => {
+  const config = {
+    headers: req.headers,
+    body: req.body,
+    method: req.method.toLowerCase(),
+    url: req.path,
+    baseURL: pointUrl,
+    auth: req.auth,
+    params: req.params,
+  };
+
   const [error, result] = await to(
-    axios.post(`${pointUrl}${req.url}`, req.body, {
-      headers: req.headers,
-      body: req.body,
-      method: req.method,
-    }),
+    req.method == 'GET'
+      ? axios.get(`${pointUrl}${req.url}`, config)
+      : req.method == 'DELETE'
+      ? axios.delete(`${pointUrl}${req.url}`, config)
+      : req.method == 'HEAD'
+      ? axios.head(`${pointUrl}${req.url}`, config)
+      : req.method == 'OPTIONS'
+      ? axios.options(`${pointUrl}${req.url}`, config)
+      : req.method == 'POST'
+      ? axios.post(`${pointUrl}${req.url}`, req.body, config)
+      : req.method == 'PUT'
+      ? axios.put(`${pointUrl}${req.url}`, req.body, config)
+      : axios.patch(`${pointUrl}${req.url}`, req.body, config),
   );
 
   if (error) {
